86f0956d3ad0843aac035feb7e172294ab855951
malbasha98 <82407513+malbasha98@users.noreply.github.com>
2024-04-23 13:03:00 +0200

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -128,4 +128,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'andrijamalbi@gmail.com'
-EMAIL_HOST_PASSWORD = 'hksn jtvd ubqc ydbk'
+EMAIL_HOST_PASSWORD = ''
